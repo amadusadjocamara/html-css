abca9c693c8f7aced6d664242481d66292b3fd08
Amadu Sadjo Camara <105577816+amadusadjocamara@users.noreply.github.com>
2023-07-05 17:57:17 +0530

    javascript

---
+++ text/javascript/demo/test-site/scripts/main02.js
@@ -0,0 +1,16 @@
+let iceCream = "chocolate";
+
+if (iceCream === "chocolate")
+{
+    alert("Yay, I love chocolate ice cream!");
+}
+else{
+    alert("Awww, but chocolate is my favorite...");
+}
+
+function multiply (num1, num2){
+    let result = num1 * num2;
+    alert(result)
+    return result;
+}
+multiply (4,7);
